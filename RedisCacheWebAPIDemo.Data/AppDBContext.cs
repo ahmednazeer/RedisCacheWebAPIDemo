5ce22470baa6d4d90441f98ced44efc28be3832c
Ahmed_Nazeer <ahmedgelany45@gmail.com>
2022-12-09 10:49:17 +0200

    Add project files.

---
+++ RedisCacheWebAPIDemo.Data/AppDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RedisCacheWebAPIDemo.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisCacheWebAPIDemo.Data
+{
+    public class AppDBContext:DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options):base(options) 
+        {
+
+        }
+        public DbSet<Driver> Drivers { get; set; }
+    }
+}
